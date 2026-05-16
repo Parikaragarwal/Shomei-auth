7f00869cf5dc4b27b68abdbb4e83928424c89703
Parikar Agarwal <parikaragarwal@gmail.com>
2026-05-16 22:43:10 +0530

    Samesite=None

--- src/routes.js
+++ src/routes.js
@@ -76,7 +76,7 @@ export async function loginHandler(req, res) {
 
     res.cookie("session_id", session_id, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       path: "/",
       secure: process.env.NODE_ENV === "production",
       maxAge: 7 * 24 * 60 * 60 * 1000,
